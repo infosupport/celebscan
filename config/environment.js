5a9bf332d7a723cfdd2aa1259405589e49b2d34c
Willem Meints <willem.meints@gmail.com>
2017-08-03 20:47:43 +0200

    Retrieve API Key from environment variable for dev

--- config/environment.js
+++ config/environment.js
@@ -29,6 +29,8 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.APP.apiKey = process.env['API_KEY'];
   }
 
   if (environment === 'test') {
