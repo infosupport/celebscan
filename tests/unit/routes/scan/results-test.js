db2dfcbb57445677756f762cd4a7263c5e53b1e4
Willem Meints <willem@mein-design.nl>
2017-08-07 08:43:53 +0200

    Fix unit-tests

--- tests/unit/routes/scan/results-test.js
+++ tests/unit/routes/scan/results-test.js
@@ -3,6 +3,7 @@ import { moduleFor, test } from 'ember-qunit';
 moduleFor('route:scan/results', 'Unit | Route | scan/results', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
+  needs: ['service:imageData', 'service:scanner']
 });
 
 test('it exists', function(assert) {
